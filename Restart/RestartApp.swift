694734c71c7deaca50f17eab0b48395ab8a1aa14
simonszabo <simimi129@gmail.com>
2022-10-25 22:00:07 +0200

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Simon Szabo on 2022. 10. 25..
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
