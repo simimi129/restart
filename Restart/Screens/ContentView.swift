cbb53d50c32f9603d0c1a6e9dbcb114031aa71a4
simonszabo <simimi129@gmail.com>
2022-10-25 22:49:04 +0200

    init

---
+++ Restart/Screens/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  Restart
+//
+//  Created by Simon Szabo on 2022. 10. 25..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+  
+  var body: some View {
+    ZStack {
+      if isOnboardingViewActive {
+        OnboardingView()
+      } else {
+        HomeView()
+      }
+    }
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
